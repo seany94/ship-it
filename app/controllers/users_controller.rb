ff6299479de42e51e2a4fd66ba67514108828f56
sean chan <sean@seans-MacBook-Air.local>
2019-02-05 16:58:48 +0800

    Change user routings and update buttons for users pages home/index/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,24 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!, :except => [ :show, :index ]
 
+  def home
+    @users = User.all
+    @jobs = nil
+    if params[:job] == '1'
+      @users = User.all
+      @jobs = Job.where(:user_id => current_user.id)
+    elsif params[:job] == '2'
+      @users = User.all
+      @jobs = Job.where(:acceptor_id => current_user.id)
+    elsif params[:job] == '3'
+      @users = User.all
+      @jobs = Job.where(:user_id => current_user.id).or(Job.where(:acceptor_id => current_user.id)).where(:completed => true)
+    elsif params[:job] == '4'
+      @users = User.all
+      @jobs = Job.where(:user_id => current_user.id).or(Job.where(:acceptor_id => current_user.id)).where(:completed => false)
+    end
+  end
+
   def index
     @users = User.all
     @jobs = nil
