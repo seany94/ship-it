68e857031269fc76ab8c74eea792c1749895c09b
kingcashthefifth <kingcashthefifth@gmail.com>
2019-02-07 18:25:51 +0800

    adjusted map default view zoom when load

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,18 +1,20 @@
 var SINGAPORE_LOCATION_OBJECT = {
-  zoom: 13,
-  center: {
-    lat: 1.3,
-    lng: 103.851959
-  }
+    zoom: 12,
+    center: {
+        lat: 1.355,
+        lng: 103.838959
+    }
 };
 var map, service, directionsRenderer, directionsService;
 
-function initAutocomplete(){
+function initAutocomplete() {
     document.addEventListener('DOMContentLoaded', () => {
         var startLocationInput = document.getElementById('job_start_location');
         var endLocationInput = document.getElementById('job_end_location');
         var options = {
-            componentRestrictions: {country: 'sg'}
+            componentRestrictions: {
+                country: 'sg'
+            }
         };
         autocompleteStart = new google.maps.places.Autocomplete(startLocationInput, options);
         autocompleteEnd = new google.maps.places.Autocomplete(endLocationInput, options);
@@ -21,7 +23,7 @@ function initAutocomplete(){
     })
 }
 
-function initMap(){
+function initMap() {
     document.addEventListener('DOMContentLoaded', () => {
         map = new google.maps.Map(document.getElementById('map'), SINGAPORE_LOCATION_OBJECT);
         directionsService = new google.maps.DirectionsService;
@@ -34,35 +36,35 @@ function initMap(){
     // });
 }
 
-function initMarkers(){
+function initMarkers() {
     service = new google.maps.places.PlacesService(map);
     gon.jobs.forEach(job => {
         let startLocationQueryParams = newQueryParams(job.start_location);
         let endLocationQueryParams = newQueryParams(job.end_location);
         service.findPlaceFromQuery(startLocationQueryParams, (sResults, sStatus) => {
             service.findPlaceFromQuery(endLocationQueryParams, (eResults, eStatus) => {
-                if (sStatus === google.maps.places.PlacesServiceStatus.OK && 
-                    eStatus === google.maps.places.PlacesServiceStatus.OK){
-                        let infoWindow = newInfoWindow(sResults[0], eResults[0].name);
-                        let marker = newMarker(sResults[0]);
-                        marker.addListener('click', () => {
-                            showRoute(directionsRenderer, directionsService, sResults[0], eResults[0]);
-                            infoWindow.open(map, marker);
-                        })
+                if (sStatus === google.maps.places.PlacesServiceStatus.OK &&
+                    eStatus === google.maps.places.PlacesServiceStatus.OK) {
+                    let infoWindow = newInfoWindow(sResults[0], eResults[0].name);
+                    let marker = newMarker(sResults[0]);
+                    marker.addListener('click', () => {
+                        showRoute(directionsRenderer, directionsService, sResults[0], eResults[0]);
+                        infoWindow.open(map, marker);
+                    })
                 }
             })
         });
     })
 }
 
-function newQueryParams(locationString){
+function newQueryParams(locationString) {
     return {
         query: locationString,
         fields: ['formatted_address', 'geometry', 'id', 'name', 'photos', 'place_id', 'plus_code', 'types']
     }
 }
 
-function newInfoWindow(result, endLocationString){
+function newInfoWindow(result, endLocationString) {
     return new google.maps.InfoWindow({
         content: `
             <div class='info-window' end-location='${endLocationString}'>
@@ -76,26 +78,30 @@ function newInfoWindow(result, endLocationString){
 }
 
 function newMarker(result) {
-  let iconOptions = {
-    url: "https://image.flaticon.com/icons/svg/46/46046.svg",
-    size: new google.maps.Size(71, 71),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(17, 34),
-    scaledSize: new google.maps.Size(30, 30)
-  };
-  return new google.maps.Marker({
-    map: map,
-    title: result.name,
-    icon: iconOptions,
-    position: result.geometry.location
-  });
+    let iconOptions = {
+        url: "https://image.flaticon.com/icons/svg/46/46046.svg",
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(30, 30)
+    };
+    return new google.maps.Marker({
+        map: map,
+        title: result.name,
+        icon: iconOptions,
+        position: result.geometry.location
+    });
 }
 
-function showRoute(directionsRenderer, directionsService, startPlace, endPlace){
+function showRoute(directionsRenderer, directionsService, startPlace, endPlace) {
     let travelMode = 'DRIVING' //WALKING, BICYCLING, TRANSIT
     let options = {
-        origin: {placeId: startPlace.place_id},
-        destination: {placeId: endPlace.place_id},
+        origin: {
+            placeId: startPlace.place_id
+        },
+        destination: {
+            placeId: endPlace.place_id
+        },
         travelMode: travelMode
     }
     directionsService.route(options, (response, status) => {
@@ -107,6 +113,6 @@ function showRoute(directionsRenderer, directionsService, startPlace, endPlace){
     });
 }
 
-function fillInAddress(){
+function fillInAddress() {
     var place = autocompleteStart.getPlace();
 }
