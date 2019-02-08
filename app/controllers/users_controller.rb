9a3678c4945f79e04481cdcc114bb246b298d916
sean chan <sean@seans-MacBook-Air.local>
2019-02-08 17:37:56 +0800

    Add updated toggleable tabs to all users and update query logics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,43 +9,27 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @jobs = Job.all
-    # if params[:job] == '1'
-    #   @users = User.all
-    #   @jobs = Job.where(:user_id => current_user.id).where(:completed => false)
-    # elsif params[:job] == '2'
-    #   @users = User.all
-    #   @jobs = Job.where(:acceptor_id => current_user.id)
-    # elsif params[:job] == '3'
-    #   @users = User.all
-    #   @jobs = Job.where(:user_id => current_user.id).or(Job.where(:acceptor_id => current_user.id)).where(:completed => true)
-    # elsif params[:job] == '4'
-    #   @users = User.all
-    #   @jobs = Job.where(:user_id => current_user.id).where.not(:acceptor_id => nil).where(:completed => false)
-    # elsif params[:job] == '5'
-    #   @users = User.all
-    #   @jobs = Job.where(:user_id => current_user.id).where(:acceptor_id => nil)
-    # end
   end
 
   def show
     @users = User.find(params[:id])
-    @jobs = nil
-    if params[:job] == '1'
-      @users = User.find(params[:id])
-      @jobs = Job.where(:user_id => @users).where(:completed => false)
-    elsif params[:job] == '2'
-      @users = User.find(params[:id])
-      @jobs = Job.where(:acceptor_id => @users)
-    elsif params[:job] == '3'
-      @users = User.find(params[:id])
-      @jobs = Job.where(:user_id => @users).or(Job.where(:acceptor_id => @users)).where(:completed => true)
-    elsif params[:job] == '4'
-      @users = User.find(params[:id])
-      @jobs = Job.where(:user_id => @users.id).where.not(:acceptor_id => nil).where(:completed => false)
-    elsif params[:job] == '5'
-      @users = User.find(params[:id])
-      @jobs = Job.where(:user_id => @users.id).where(:acceptor_id => nil)
-    end
+    @jobs = Job.all
+    # if params[:job] == '1'
+    #   @users = User.find(params[:id])
+    #   @jobs = Job.where(:user_id => @users).where(:completed => false)
+    # elsif params[:job] == '2'
+    #   @users = User.find(params[:id])
+    #   @jobs = Job.where(:acceptor_id => @users)
+    # elsif params[:job] == '3'
+    #   @users = User.find(params[:id])
+    #   @jobs = Job.where(:user_id => @users).or(Job.where(:acceptor_id => @users)).where(:completed => true)
+    # elsif params[:job] == '4'
+    #   @users = User.find(params[:id])
+    #   @jobs = Job.where(:user_id => @users.id).where.not(:acceptor_id => nil).where(:completed => false)
+    # elsif params[:job] == '5'
+    #   @users = User.find(params[:id])
+    #   @jobs = Job.where(:user_id => @users.id).where(:acceptor_id => nil)
+    # end
   end
 
   def new
