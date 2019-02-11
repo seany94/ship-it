620b3937b3c9e9eb384d23cf3eb76696ce029496
kingcashthefifth <kingcashthefifth@gmail.com>
2019-02-12 01:44:17 +0800

    done css adjustment for maps view page

--- app/assets/javascripts/design.js
+++ app/assets/javascripts/design.js
@@ -1,32 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     setActiveNavItem();
-    if (document.querySelector('#login_new_user')){
+    if (document.querySelector('#login_new_user')) {
         $(login_new_user).on('ajax:error', loginAjaxHandler)
     }
 
 });
 
-function loginAjaxHandler(response){
-    if (!document.querySelector('#login-error')){
+function loginAjaxHandler(response) {
+    if (!document.querySelector('#login-error')) {
         $(login_new_user).prepend('<div class="alert alert-danger" id="login-error">' + response.detail[0].error + '</div>');
     }
 }
 
-function getRouteString(windowInstance, urlString){
+function getRouteString(windowInstance, urlString) {
     return urlString.split(windowInstance.location.origin).join('').split('/')[1];
 }
 
-function setActiveNavItem(){
+function setActiveNavItem() {
     let currentRouteString = getRouteString(window, window.location.href);
     document.querySelectorAll('li.nav-item').forEach(li => {
-        if (currentRouteString === getRouteString(window, li.childNodes[1].href)){
+        if (currentRouteString === getRouteString(window, li.childNodes[1].href)) {
             li.classList.add('active');
         } else {
             li.classList.remove('active');
         }
-    })    
+    })
 }
-function addSlide(){
+
+function addSlide() {
     document.querySelector('#job-list').classList = 'job-list-transition';
     document.querySelector('#map-wrapper').classList = 'map-wrapper-transition';
 }
